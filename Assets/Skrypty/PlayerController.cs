eef684a233497da8b677d5d49261687792133a2c
Nikodemus <nikodembilinski4@gmail.com>
2025-12-02 13:44:07 +0100

    test githuba - dodanie komentarza

--- Assets/Skrypty/PlayerController.cs
+++ Assets/Skrypty/PlayerController.cs
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
     private bool isgrounded;
     
 
-    
+    // test test github test
    
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
